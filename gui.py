0b923df4082d23303bdadeeb04c8f90356b19aa1
jieyuzhou42 <jieyuz@andrew.cmu.edu>
2025-03-11 14:39:00 -0400

    delete repeated web interface functions

--- gui.py
+++ gui.py
@@ -19,10 +19,14 @@ class ValAgent:
 
 
 user_interface = WebInterface()
-user_task = user_interface.request_user_task()
+
 tasks = HtnInterface("knowledge_base.json")
 ValAgent = ValAgent
 # VAL interpret user task
+# initial state:
+# what does the start point looks like? current design is based on learned HTN knowledge/AI generated plan
+# But when users 
+user_task = user_interface.request_user_task()
 # task = ValAgent.interprete(user_task)
 decomposition = tasks.get_decomposition(0)
 i = 1
@@ -39,3 +43,7 @@ while decomposition:
         decomposition = tasks.get_decomposition(4)
         # After rendering the chat box with VAL, VAL learns how to do the task. 
         # The HTN will send the same format decomposition to the front end, and then reactflow will render it.
+    elif confirm_result == "edit":
+        # 
+        pass
+    
